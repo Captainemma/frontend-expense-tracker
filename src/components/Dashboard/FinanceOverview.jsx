a0aef08fda8351b6810f332c147ee07952a2193d
Captainemma <emmaamokwandh@gmail.com>
2025-08-20 09:11:18 +0000

    Made slight chnages

--- src/components/Dashboard/FinanceOverview.jsx
+++ src/components/Dashboard/FinanceOverview.jsx
@@ -19,7 +19,7 @@ const FinanceOverview = ({ totalBalance, totalIncome, totalExpense }) => {
       <CustomPieChart
         data={balanceData}
         label="Total Balance"
-        totalAmount={`$${totalBalance}`}
+        totalAmount={`GH₵${totalBalance}`}
         colors={COLORS}
         showTextAnchor
       />
--- src/components/Dashboard/RecentIncomeWithChart.jsx
+++ src/components/Dashboard/RecentIncomeWithChart.jsx
@@ -30,7 +30,7 @@ const RecentIncomeWithChart = ({ data, totalIncome }) => {
       <CustomPieChart
         data={chartData}
         label="Total Income"
-        totalAmount={`$${totalIncome}`}
+        totalAmount={`GH₵${totalIncome}`}
         showTextAnchor
         colors={COLORS}
       />
